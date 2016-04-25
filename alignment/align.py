e7b539cc60c16aef6d37ba1aae039939e4b7973e
Eric Bannatyne <eric.bannatyne@gmail.com>
2016-04-25 18:54:54 -0400

    Add starting files for alignment and lip extraction.

---
+++ alignment/align.py
@@ -0,0 +1,6 @@
+import nltk
+
+words = ['place', 'green', 'on', 'g', 'three', 'now']
+phoneme_dict = nltk.corpus.cmudict.dict()
+
+
---
+++ lip-extraction/extract_lips.py
@@ -0,0 +1,46 @@
+import os
+from scipy.io import loadmat
+
+import numpy as np
+#import matplotlib.pyplot as plt
+
+import cv2
+
+LIPS_PATH = "/Users/eric/Downloads/avletters/Lips"
+
+mouth_vids = []
+
+cc = cv2.CascadeClassifier("haarcascade_mcs_mouth.xml")
+
+rect_area = lambda rect: (rect[2] - rect[0]) * (rect[3] - rect[1])
+
+for fname in os.listdir(LIPS_PATH):
+    if fname.endswith(".mat"):
+        vid = loadmat(LIPS_PATH + "/" + fname)['vid'].reshape((80, 60, -1)).transpose(1, 0, 2)
+        print vid.shape
+
+        frame = 0
+
+        mouth = cv2.cvtColor(vid[:, :, frame], cv2.COLOR_GRAY2BGR)
+
+        rects = cc.detectMultiScale(mouth, minNeighbors=1)
+        """
+        for rect in cc.detectMultiScale(mouth, minNeighbors=1):
+            print rect
+            cv2.rectangle(mouth, (rect[0], rect[1]), (rect[2], rect[3]), (125, 125, 25), 2)
+        """
+
+        mouth_rect = max(rects, key=rect_area)
+        cv2.rectangle(mouth, (mouth_rect[0], mouth_rect[1]), (mouth_rect[2], mouth_rect[3]), (125, 125, 25), 1)
+        print type(mouth_rect)
+        print rect_area(mouth_rect)
+
+        #plt.imshow(vid[:, :, 0], cmap=plt.cm.gray, interpolation=None)
+        #plt.show()
+
+        cv2.imshow('detected mouths', mouth)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+
+
