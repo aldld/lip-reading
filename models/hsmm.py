0fe15735710438a8cd59bcf4d842219433191ecd
Eric Bannatyne <eric.bannatyne@gmail.com>
2016-04-28 13:21:04 -0400

    Refactor hsmmy.py

--- models/hsmm.py
+++ models/hsmm.py
@@ -5,8 +5,8 @@ from scipy.io import loadmat
 from sklearn.mixture import GMM
 import pyhsmm
 
-def train_word_init_probs(data, vocab):
-    word_init_counts = {word: 0 for word in vocab}
+def train_word_init_probs(data, vocab_size):
+    word_init_counts = np.zeros((vocab_size,))
 
     for chain in data:
         if chain['state_seq'][0] in word_init_counts:
@@ -14,11 +14,12 @@ def train_word_init_probs(data, vocab):
         else:
             word_init_counts[chain['state_seq'][0]] += 1
 
-    return {word: float(count) / len(vocab) for word, count in word_init_counts}
+    #return {word: float(count) / len(vocab) for word, count in word_init_counts}
+    return word_init_counts / float(vocab_size)
 
-def train_word_trans_probs(data, vocab):
+def train_word_trans_probs(data, vocab_size):
     """ Return the HSMM state transition matrix trained using MLE from bigram counts. """
-    bigram_counts = np.zeros((len(vocab), len(vocab)))
+    bigram_counts = np.zeros((vocab_size, vocab_size))
 
     num_bigrams_tot = 0
     for chain in data:
@@ -28,10 +29,10 @@ def train_word_trans_probs(data, vocab):
 
     return bigram_counts / float(num_bigrams_tot)
 
-def train_word_durations(data, vocab):
+def train_word_durations(data, vocab_size):
     """ Learn lambda parameter for word durations via MLE, assuming that durations follow a Poisson distribution. """
-    word_counts = np.asarray([0.0 for _ in vocab])
-    word_durations = np.asarray([0.0 for _ in vocab])
+    word_counts = np.asarray([0.0 for _ in xrange(vocab_size)])
+    word_durations = np.asarray([0.0 for _ in xrange(vocab_size)])
 
     for chain in data:
         for idx, word in enumerate(chain['state_seq']):
@@ -42,17 +43,17 @@ def train_word_durations(data, vocab):
 
     return word_durations / word_counts
 
-def gather_gmm_data(data, vocab):
+def gather_gmm_data(data, vocab_size):
     """ Returns a dictionary mapping words to their observation data matrices (of shape (num_segments, hogs_dim)) """
     # Collect segments for each word in training data.
-    segments = [{} for _ in vocab]
+    segments = [{} for _ in xrange(vocab_size)]
     for chain in data:
         for idx, word in enumerate(chain['state_seq']):
             segments[word].add(chain['obs'][idx])
 
     # Put segments for each word together, so that each word has a single data matrix.
     train_data_gmm = {}
-    for word in vocab:
+    for word in xrange(vocab_size):
         train_data_gmm[word] = np.vstack((np.asarray(seg) for seg in segments[word])).T
 
     return train_data_gmm
@@ -67,11 +68,14 @@ def train_word_gmms(train_data_gmm, n_components=6):
 def build_hsmm(word_init_probs, word_trans_probs, word_dur_params, word_gmms):
     return
 
-def train_hsmm(data, n_components=6):
+def save_params(word_init_probs, word_trans_probs, word_dur_params, word_gmms):
+    pass
+
+def train_hsmm(data, vocab_size, n_components=6, save_intermediate=False):
     """ Trains a HSMM from the given complete data.
 
-        data: Set of observation sequences and state sequences.
-            {
+        data: List of observation sequences and state sequences.
+            [
                 {
                     state_seq: [word_0, word_1, word_2]
                     obs: [[frame_0, frame_1], [frame_2, frame_3, frame_4], [frame_5]],
@@ -81,21 +85,22 @@ def train_hsmm(data, n_components=6):
                     obs: [[frame_0, frame_1], [frame_2, frame_3, frame_4], [frame_5]],
                 }
                 ...
-            }
+            ]
     """
 
-    vocab = {} # Set of all words in the vocabulary.
-
-    word_init_probs = train_word_init_probs(data, vocab) # Compute initial word probabilities.
+    word_init_probs = train_word_init_probs(data, vocab_size) # Compute initial word probabilities.
     
-    word_trans_probs = train_word_trans_probs(data, vocab) # Compute word transition probabilities.
-    word_dur_params = train_word_durations(data, vocab) # Learn parameters of word duration distributions.
+    word_trans_probs = train_word_trans_probs(data, vocab_size) # Compute word transition probabilities.
+    word_dur_params = train_word_durations(data, vocab_size) # Learn parameters of word duration distributions.
 
     # Gather data for training each GMM.
-    train_data_gmm = gather_gmm_data(data, vocab)
+    train_data_gmm = gather_gmm_data(data, vocab_size)
 
     # Train word GMMs.
     word_gmms = train_word_gmms(train_data_gmm, n_components=n_components)
+
+    if save_intermediate:
+        save_params(word_init_probs, word_trans_probs, word_dur_params, word_gmms)
 
     return build_hsmm(word_init_probs, word_trans_probs, word_dur_params, word_gmms)
 
